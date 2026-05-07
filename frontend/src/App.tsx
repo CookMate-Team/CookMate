2a34e044165bfe0b53cba6460a089c4a13db6bb9
Filipwasiel <76472716+Filipwasiel@users.noreply.github.com>
2026-05-08 00:26:29 +0200

    Basic main page structure, without any functionality for now just a skeleton

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow max-w-7xl mx-auto w-full px-4 sm:px-6 lg:px-8 py-8">
+        <div className="text-center mb-8">
+          <h1 className="text-4xl font-extrabold text-stone-800 tracking-tight">What are you craving?</h1>
+          <p className="mt-4 text-lg text-stone-500 max-w-2xl mx-auto">
+            Explore our vast collection of recipes or discover something entirely new to cook today.
+          </p>
+        </div>
+
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
