05f2c27fca3604d109368705686fe34cf6ab5872
blanqtoja <156962427+blanqtoja@users.noreply.github.com>
2026-05-16 11:09:37 +0200

    import changed

--- main-service/src/main/java/com/cookmate/main/service/StepService.java
+++ main-service/src/main/java/com/cookmate/main/service/StepService.java
@@ -14,7 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import tools.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.List;
 import java.util.stream.Collectors;
